7c8636697987711e7914a102fd2936d629e1658c
Hatice ERTÜRK <haticeerturk27@gmail.com>
2017-12-20 09:53:51 +0300

    Done the issues

--- views.py
+++ views.py
@@ -1,14 +1,17 @@
+import aiofiles
+import asyncio
 import csv
 from aiohttp import web
+from utils import upload
 
 routes = web.RouteTableDef()
 
 @routes.get('/api/employee/')
 async def handle_get(request):
-    with open('Employees.csv') as file:
-        reader = csv.reader(file, delimiter=';')
+    async with aiofiles.open('Employees.csv', newline='') as file:
         employees = []
-        for row in reader:
+        async for line in file:
+            row = line.split(',')
             employees.append({
                 'name': row[0],
                 'surname': row[1],
@@ -20,8 +23,9 @@ async def handle_get(request):
 @routes.post('/api/employee/')
 async def handle_post(request):
     data = await request.json()
-    str_data = data['name']+';'+data['surname']+';'+data['mail']+';'+data['birthday']
 
-    with open('Employees.csv', 'a') as file:
-        file.write(str_data)
-        return web.json_response(data)
+    async with aiofiles.open('Employees.csv', mode='a') as file:
+        writer = csv.writer(file)
+        await writer.writerow(data.values())
+
+    return web.json_response(data)
